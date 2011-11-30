16a36fdaf21ae053acdac5c6198a355b60b16a81
Voidcode <terkelsorensen@gmail.com>
2011-11-30 12:14:15 +0100

    fix bug 1: ShareActivity just load do not stop loading

--- src/com/voidcode/diasporawebclient/ShareActivity.java
+++ src/com/voidcode/diasporawebclient/ShareActivity.java
@@ -1,13 +1,11 @@
 package com.voidcode.diasporawebclient;
 
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 
 public class ShareActivity extends MainActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        	startDiasporaBrowser("/status_messages/new");
+        	mWeb.loadUrl(main_domain+"/status_messages/new");
     }
 }
